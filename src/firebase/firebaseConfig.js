98e17fabc49d9e4389cdcfc07db4bb6bfc4a3f3a
Masuma Alizadeh <masumaal@kth.se>
2026-04-16 12:51:33 +0200

    config

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCepQ9H78YPIsO23_4iBmBx1scEFrCyTqU",
+  authDomain: "codatrail-c6373.firebaseapp.com",
+  projectId: "codatrail-c6373",
+  storageBucket: "codatrail-c6373.firebasestorage.app",
+  messagingSenderId: "107988285669",
+  appId: "1:107988285669:web:6569939a0a93f1c1977c0c",
+  measurementId: "G-PQHYWQFBB8"
+};
